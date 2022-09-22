e4751ce2c64edc51d95cee8d3535bf4b86e19eb9
Maryna-Bazylevska <83463971+Maryna-Bazylevska@users.noreply.github.com>
2022-09-22 16:07:23 +0200

    add

---
+++ routes/api/auth.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+router.post("singup", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,14 @@ require("dotenv").config();
 const { DB_HOST, PORT = 3000 } = process.env;
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT))
-  .catch((error) => {
-    console.log(error.message);
+  .then(() => {
+    console.log("DB is working");
+    app.listen(PORT);
+  })
+  .then(() => {
+    console.log(`Server is on ${PORT}`);
+  })
+  .catch((err) => {
+    console.log("ERROR", err);
     process.exit(1);
   });
